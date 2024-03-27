c9284e80117d83f4324279fda9b071e3b4197a42
Ewing <cyyooooo00@gmail.com>
2024-03-28 00:35:29 +0800

    增加了多线程下载数据的方式

--- cspn_pytorch/datalist/processing.py
+++ cspn_pytorch/datalist/processing.py
@@ -7,12 +7,11 @@ from pathlib import Path
 import requests
 from concurrent.futures import ThreadPoolExecutor
 
-def download_file(link, save_dir):
+def download_file(link, save_path):
     filename = link.split('/')[-1]
-    filepath = os.path.join(save_dir, filename)
     response = requests.get(link)
     if response.status_code == 200:
-        with open(filepath, 'wb') as f:
+        with open(save_path, 'wb') as f:
             f.write(response.content)
 
 def download_files(links, save_dir):
@@ -21,7 +20,8 @@ def download_files(links, save_dir):
         return
     with ThreadPoolExecutor(max_workers=10) as executor:  # 控制同时进行下载的线程数
         for link in links:
-            executor.submit(download_file, link, save_dir)
+            save_path=Path(save_dir)/link.split('/')[-2]/link.split('/')[-1]
+            executor.submit(download_file, link, save_path)
 
 base_url='http://datasets.lids.mit.edu/nyudepthv2/nyudepthv2_noskip/val_full/'
 
@@ -33,12 +33,12 @@ with open(Path(__file__).parent/'nyudepth_hdf5_train.csv','r') as file:
         links.append(base_url+line.strip().split('/')[-2]+'/'+line.strip().split('/')[-1])
         count+=1
         if count>=1000:
-            download_files(links=links,save_dir=Path(__file__).parent.parent/'data')
+            download_files(links=links,save_dir=Path(__file__).parent.parent/'data/nyudepth_hdf5/train')
             print(count,end=' ')
             count=0
             links=[]
             pass
-    download_files(links=links,save_dir=Path(__file__).parent.parent/'data')
+    download_files(links=links,save_dir=Path(__file__).parent.parent/'data/nyudepth_hdf5/train')
     print(count,end=' ')
 
     
